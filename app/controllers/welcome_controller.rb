3725dabca127f71be4bc5ea868e204775e3f6f2b
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-06-25 13:17:29 -0400

    Done.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+    before_action :logged_in?
+
+    def home
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
+  root 'welcome#home'
+
+
 end
